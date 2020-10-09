ee4578ade90729676d2ce0656aeb4c30abbd0a02
104314 <69081965+104314@users.noreply.github.com>
2020-10-09 13:42:35 +0530

    Update main.c

--- implementation/main.c
+++ implementation/main.c
@@ -1,1 +1,23 @@
+#include<atmoperations.h>
 
+int main()
+
+{
+
+   float x;
+
+   char ch;
+
+   printf("Enter initial amount\n");
+
+   scanf("%f",&x);
+
+   printf("Enter\n for credit\n for debit\n for balance\n");
+
+   scanf("%c",&ch);
+
+   atm(ch,x);
+
+   return 0;
+
+}
